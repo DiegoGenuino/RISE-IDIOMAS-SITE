ced507c3da80071120ed22b304019fb666ab0bc8
DiegoGenuino <genuinodiego11@gmail.com>
2026-05-09 02:40:09 -0300

    fix: correct import statement for vercel in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,8 @@
 import { defineConfig } from "astro/config";
 import icon from "astro-icon";
 import tailwindcss from "@tailwindcss/vite";
+import vercel from "@astrojs/vercel";
 import react from "@astrojs/react";
-import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
 export default defineConfig({
